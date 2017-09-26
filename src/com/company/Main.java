93ed9d3378a462267158dbe84507ecf8ba8d6e70
mstomel <jajalope372@gmail.com>
2017-09-26 08:59:14 -0400

    v1.0

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,44 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
 
-        char a;
-        int opp;
         Scanner in = new Scanner(System.in);
         System.out.println("What is your word");
         String word = in.next();
-        if (word.length() % 5 == 0) {
+        int opp = (int)(Math.random()*word.length());
 
-            int opp = 5;
-
-        } else if (word.length() % 4 == 0) {
-
-            int opp = 4;
-
-        }else if (word.length() % 3 == 0) {
-
-            int opp = 3;
-
-        }else if (word.length() % 2 == 0) {
-
-            int opp = 2;
-
-        }else if (word.length() % 1 == 0) {
-
-            int opp = 1;
-
-        }else {
-
-            System.out.println("invalid input");
-
-        }
-
-        int count = 0;
         for (int i = 0; i<word.length(); i++){
 
-            word.substring(i, i+1)=a;
             if (opp % 5 == 0){
-                
-                System.out.println();
+
+
+                System.out.print(word.charAt(i));
+                opp++;
+
+            }else if (opp % 4 == 0) {
+
+                System.out.print(Integer.toHexString(((int) word.charAt(i))));
+                opp++;
+
+            }else if (opp % 3 == 0) {
+
+                System.out.print((int) word.charAt(i));
+                opp++;
+
+            }else if (opp % 2 == 0)  {
+
+                System.out.print(Integer.toOctalString(((int) word.charAt(i))));
+                opp++;
+
+            }else if (opp % 1 == 0) {
+
+                System.out.print(Integer.toBinaryString(((int) word.charAt(i))));
+                opp++;
+
+            }else {
+
+                System.out.println("error");
 
             }
 
@@ -51,3 +49,4 @@ public class Main {
 
     }
 }
+//slight method help credit to Luke
